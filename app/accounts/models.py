16a4d1fcc3b1e0daf6e8e983f163e2fe3709e783
Laurene Connelly <laureen.connelly@gmail.com>
2022-06-21 20:53:36 -0700

    model updates

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    account_no = models.CharField(max_length=70, blank=False, default='')
+    account_name = models.CharField(max_length=200, blank=False, default='')
--- app/expenses/models.py
+++ app/expenses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):
+    date = models.CharField(max_length=70, blank=False, default='')
+    amount = models.CharField(max_length=200, blank=False, default='')
+    attachment = models.CharField(max_length=70, blank=True, null=True)
+    note = models.CharField(max_length=70, blank=True, null=True)
+    account_id = models.CharField(max_length=70, blank=False, default='')
+    employee_id = models.CharField(max_length=70, blank=False, default='')
+    department_id = models.CharField(max_length=70, blank=False, default='')
